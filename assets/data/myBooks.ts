48252f13653880ecaea6013daebd1358a862447f
kansukht <52836778+kansukht@users.noreply.github.com>
2019-07-30 00:38:36 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,14 @@
+import {books} from '../../app/books';
+import { HtmlTagDefinition } from '@angular/compiler';
+
+export const MYBOOKS: books[] = 
+[
+{sbookn:'War and Peace', sauthor: 'Leo Tolstoy', sgenre: 'Historical Novel', syearpub: 1869 , spic :'assets/images/war.jpg'},
+{sbookn:'The Lord of THe Rings', sauthor: 'J.R.R. Tolkein ', sgenre: 'Fantasy', syearpub: 1954, spic: 'assets/images/lord.png'},
+{sbookn:'The Shadow of The Wind', sauthor: 'Charlos Ruiz Zafon', sgenre: 'Mystery', syearpub: 2001, spic: 'assets/images/wind.jpg'},
+{sbookn:'Catch-22', sauthor: 'Joseph Heller', sgenre: 'Dark Comedy', syearpub: 1961 , spic: 'assets/images/22.jpg'}
+
+
+
+
+]
